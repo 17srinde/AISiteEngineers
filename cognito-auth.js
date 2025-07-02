3aa45ea9404a17da8ae08a63dbed99905adb6f9f
17srinde <159750284+17srinde@users.noreply.github.com>
2025-07-01 19:47:05 -0700

    Update cognito-auth.js

--- cognito-auth.js
+++ cognito-auth.js
@@ -27,3 +27,36 @@ const registerUser = () => {
   });
 };
 
+const signInUser = () => {
+  const email = document.getElementById('emailInput').value;
+  const password = document.getElementById('passwordInput').value;
+
+  const authenticationData = {
+    Username: email,
+    Password: password
+  };
+
+  const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+  const userPool = new AmazonCognitoIdentity.CognitoUserPool({
+    UserPoolId: _config.cognito.userPoolId,
+    ClientId: _config.cognito.userPoolClientId
+  });
+
+  const userData = {
+    Username: email,
+    Pool: userPool
+  };
+
+  const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: (result) => {
+      const idToken = result.getIdToken().getJwtToken();
+      console.log("Login successful. Token:", idToken);
+    },
+    onFailure: (err) => {
+      alert(err.message || JSON.stringify(err));
+    }
+  });
+};
+
